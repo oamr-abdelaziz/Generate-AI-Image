3b94f6d7e9856e58dc86dc28ee0bf70d4aea0271
oamr-abdelaziz <76872117+oamr-abdelaziz@users.noreply.github.com>
2024-05-13 13:16:09 +0300

    adding project to github

---
+++ app/generate-image.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GenerateImage = () => {
+  return (
+    <View>
+      <Text>generate-image</Text>
+    </View>
+  )
+}
+
+export default GenerateImage
+
+const styles = StyleSheet.create({})
---
+++ app/generation-success.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GenerationSuccess = () => {
+  return (
+    <View>
+      <Text>generation-success</Text>
+    </View>
+  )
+}
+
+export default GenerationSuccess
+
+const styles = StyleSheet.create({})
